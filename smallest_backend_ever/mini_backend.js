8aa6d84e82e90407c20df735710eabd4f2e22156
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2022-07-17 22:51:22 +0200

    fix url

--- smallest_backend_ever/mini_backend.js
+++ smallest_backend_ever/mini_backend.js
@@ -1,5 +1,5 @@
 let jsonFromServer = {};
-let BASE_SERVER_URL = 'https://marcos-tech.de/smallest_backend_ever';
+let BASE_SERVER_URL = 'https://join.marcos-tech.de/smallest_backend_ever';
 
 const backend = {
     setItem: function(key, item) {
